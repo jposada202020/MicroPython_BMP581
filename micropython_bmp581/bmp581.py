8db032437d127d08cb8c880a63bc2d77d24ab529
jposada202020 <jquintana202020@gmail.com>
2023-06-18 11:04:11 -0400

    update

--- micropython_bmp581/bmp581.py
+++ micropython_bmp581/bmp581.py
@@ -77,7 +77,7 @@ class BMP581:
 
     .. code-block:: python
 
-        i2c = I2C(sda=Pin28), scl=Pin(3))
+        i2c = I2C(sda=Pin(2), scl=Pin(3))
         bmp581 = bmp581.BMP581(i2c)
 
     Now you have access to the attributes
